a0f3659487814ccf75c700a4782999ddad130d0b
dhotters <57628184+dhotters@users.noreply.github.com>
2019-11-13 15:56:56 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ from math import *
 # Include all functions
 import WingLoadFunction
 
-# Calculate total loading
-# NOTE here the mass of the fuel is devided by 2 because else the fuel tank is bigger then the span of the wing
-# NOTE the positions of the engine i could not find hence they are random numbers for now
-# NOTE the wing loading takes into account the weight of the wing, the fuel, the engines and the wingtip
 xAr, zAr, M_Y = WingLoadFunction.WingLoad(massFuel=104790*0.588, accuracy=10000)
 
 # optionally print the result
