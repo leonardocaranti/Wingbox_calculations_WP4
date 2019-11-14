e9d948f1546a5aae4e89068cb7df2a59cbbe3f74
ilseputs <57632926+ilseputs@users.noreply.github.com>
2019-11-14 20:11:00 +0100

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,10 @@
+#graph calculation
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def force_diagrams(force, xspan):
+    plt.plot(xspan,force)
+    plt.xlabel("x-coordinate span (m)")
+    plt.ylabel("shear force (kN)")
+    plt.show()
