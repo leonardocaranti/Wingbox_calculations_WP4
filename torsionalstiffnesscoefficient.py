9ec73a27f26313736598d347bee646572c37166f
TiesH11 <57628513+TiesH11@users.noreply.github.com>
2019-11-25 17:34:25 +0100

    Add files via upload

--- torsionalstiffnesscoefficient.py
+++ torsionalstiffnesscoefficient.py
@@ -24,24 +24,34 @@ def torsionalstiffness():
     b_2 = 28.74 		#[m]
 
     span_tab =[]
-    t_stiffness_tab = []
+    
 
     (x,y) = Momentvectors()
-
+    span_tab = []
+    t_stiffness_tab = []
+    t_stiffness_tab2 = []
+    x1 = 0
     for span_position in x:
+
+        if span_position > 0:
             t, theta, dist, front_spar_h, rear_spar_h, stringer_height, stringer_thickness = moi.initial_values(span_position)
-        
+            
             A_m = ((rear_spar_h+front_spar_h)/2)*dist
             intdst = (front_spar_h/t + rear_spar_h/t + sqrt((front_spar_h - rear_spar_h)**2 + (dist*dist)) + dist)/t
-            t_stiffness = (4*A_m*A_m*G)/(b_2*intdst)
+            t_stiffness = (4*A_m*A_m*G)/(abs(span_position-x1)*intdst)
             span_tab.append(span_position)
             t_stiffness_tab.append(t_stiffness)
+            x1 = span_position
 
-    return(span_tab, t_stiffness_tab)
 
-(span_tab, t_stiffness_tab) = torsionalstiffness()
-plt.plot(span_tab,t_stiffness_tab)
-plt.title("Torsional stiffness")
+            t_stiffness2 = (4*A_m*A_m*G)/(intdst)
+            t_stiffness_tab2.append(t_stiffness2/b_2)
+    return(span_tab, t_stiffness_tab, t_stiffness_tab2)
+
+(span_tab, t_stiffness_tab, t_stiffness_tab2) = torsionalstiffness()
+plt.plot(span_tab,t_stiffness_tab2)
+plt.title("Sectional torsional stiffness")
 plt.xlabel("Spanwise location from root [m]")
-plt.ylabel("Torsional stiffness T/Theta")
+plt.ylabel("Torsional stiffness T/Theta [N/rad]")
 plt.show()
+
