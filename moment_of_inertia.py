067dedd9bea75ab3e0f330ba0594ee00a6c01b5b
leonardocaranti <51086800+leonardocaranti@users.noreply.github.com>
2019-12-01 20:27:51 +0100

    Update moment_of_inertia.py

--- moment_of_inertia.py
+++ moment_of_inertia.py
@@ -18,32 +18,43 @@ front_spar_h_root = 10.29/100*root_chord
 root_dist = 0.45*root_chord
 theta_root = atan((front_spar_h_root-rear_spar_h_root)*0.5/root_dist)
 
-"""Case 1
-t0, t1 = 0.07, 0.03
+#Case 1
+t0, t1 = 0.07, 0.03 
 stringer_height_0, stringer_height_1 = 0.12, 0.12
 stringer_thickness_0, stringer_thickness_1 = 0.03, 0.03
-cross_section_value = 1                                     # 1 corresponds to a cross section of skins only, 2 is skins + stringers on the top, 3 is stringers on top and bottom
-no_stringers_top = 5                                        # Must be greater than 1 for code to work!
-no_stringers_bott = 3                                       # Must be greater than 1 for code to work!
-"""
+cross_section_value = 1                                     # 1 corresponds to a cross section of skins only + one central spar, 2 is skins + stringers on the top, 3 is stringers on top and bottom
+no_stringers_top = 35                                        # Must be greater than 1 for code to work!
+no_stringers_bott = 25                                       # Must be greater than 1 for code to work!
 
-"""Case 2
+
+"""
+#Case 2
 t0, t1 = 0.03, 0.03
 stringer_height_0, stringer_height_1 = 0.05, 0.05
 stringer_thickness_0, stringer_thickness_1 = 0.01, 0.01
-cross_section_value = 2                                     # 1 corresponds to a cross section of skins only, 2 is skins + stringers on the top, 3 is stringers on top and bottom
+cross_section_value = 2                                     # 1 corresponds to a cross section of skins only + one central spar, 2 is skins + stringers on the top, 3 is stringers on top and bottom
 no_stringers_top = 14                                        # Must be greater than 1 for code to work!
 no_stringers_bott = 5                                       # Must be greater than 1 for code to work!
 """
 
-
+"""
+#Case 3
 t0, t1 = 0.03, 0.03
 stringer_height_0, stringer_height_1 = 0.05, 0.05
 stringer_thickness_0, stringer_thickness_1 = 0.01, 0.01
-cross_section_value = 3                                     # 1 corresponds to a cross section of skins only, 2 is skins + stringers on the top, 3 is stringers on top and bottom
+cross_section_value = 3                                     # 1 corresponds to a cross section of skins only + one central spar, 2 is skins + stringers on the top, 3 is stringers on top and bottom
 no_stringers_top = 10                                        # Must be greater than 1 for code to work!
 no_stringers_bott = 5                                       # Must be greater than 1 for code to work!
+"""
 
+"""Random
+t0, t1 = 0.02, 0.01
+stringer_height_0, stringer_height_1 = 0.012, 0.12
+stringer_thickness_0, stringer_thickness_1 = 0.01, 0.01
+cross_section_value = 2                                     # 1 corresponds to a cross section of skins only, 2 is skins + stringers on the top, 3 is stringers on top and bottom
+no_stringers_top = 9                                        # Must be greater than 1 for code to work!
+no_stringers_bott = 4                                       # Must be greater than 1 for code to work!
+"""
 
 class beam:
 
@@ -170,6 +181,9 @@ def MOI(span_position):
         
     return moi_xx, moi_yy, moi_xy
 
+def J(span_position):
+    moi_xx, moi_yy, moi_xy = MOI(span_position)
+    return moi_xx + moi_yy
 
 def plot_cross_section(span_position):
 
@@ -182,10 +196,10 @@ def plot_cross_section(span_position):
         plt.plot(element.x_coords, element.y_coords, color = colour)
 
     centr = centroid(span_position)
-    plt.axvline(x=centr[0], lw=1, ls='dashed', color = "black")
-    plt.axhline(y=centr[1], lw=1, ls='dashed', color = "black")
+    #plt.axvline(x=centr[0], lw=1, ls='dashed', color = "black")
+    #plt.axhline(y=centr[1], lw=1, ls='dashed', color = "black")
     plt.title(label = "Cross-section at a span position of " + str(span_position) + "[m]")
-    plt.text(centr[0]*10, centr[1]*10, "Centroid: (" + str(round(centr[0],2)) + "," + str(round(centr[1],2)) + ")", color = "green")
+    #plt.text(centr[0]*10, centr[1]*10, "Centroid: (" + str(round(centr[0],2)) + "," + str(round(centr[1],2)) + ")", color = "green")
 
     plt.show()
 
@@ -224,66 +238,3 @@ def mass():
 
     avg_area = (area_root + area_tip)/2
     return density*avg_area
-
-"""
-# General values
-spar_thickness = 0.03
-b_2 = 28.74
-
-# Tip
-rear_spar_h_tip = 0.2324
-front_spar_h_tip = 0.2534
-tip_dist = 0.891
-
-# Root
-rear_spar_h_root = 0.8606
-front_spar_h_root = 0.9386
-root_dist = 3.3
-
-# Calculate dimensions at given span
-def dimens(span_position):
-
-    rear_spar_h = span_position*(rear_spar_h_tip-rear_spar_h_root)/b_2 + rear_spar_h_root
-    front_spar_h = span_position*(front_spar_h_tip-front_spar_h_root)/b_2 + front_spar_h_root
-    dist = span_position*(tip_dist-root_dist)/b_2 + root_dist
-
-    return [rear_spar_h, front_spar_h, dist]
-
-# Centroid
-def centroid(span_position):
-
-    rear_spar_h, front_spar_h, dist = dimens(span_position)
-
-    # Calculate centroid
-    centr_y = (rear_spar_h*spar_thickness*dist + dist*spar_thickness*dist*0.5*2)/(rear_spar_h*spar_thickness + front_spar_h*spar_thickness + dist*spar_thickness*2)
-    centr_z = 0
-
-    return [centr_y, centr_z]
-
-
-# Moment of inertia
-def MOI(span_position):
-
-    rear_spar_h, front_spar_h, dist = dimens(span_position)
-
-    # Calcualte moment of inertia
-    I_x = front_spar_h*spar_thickness*(centroid(span_position)[0])**2 + rear_spar_h*spar_thickness*(dist-centroid(span_position)[0])**2 + 2*dist*spar_thickness*(dist/2-centroid(span_position)[0])**2
-    I_y = 2*dist*spar_thickness*((front_spar_h-rear_spar_h)/4+0.5*rear_spar_h)**2
-
-    return [I_x,I_y]
-
-# Local cross-sectional area
-def local_area(span_position):
-
-    rear_spar_h, front_spar_h, dist = dimens(span_position)
-
-    area = (rear_spar_h+front_spar_h)/2*dist
-
-    return area
-
-
-print("Centroid location", centroid(0))
-print()
-print("Moments of inertia", MOI(0))
-print("Area", local_area(0))
-"""
