555d25e9c4690461ca32e07ba2adc00759e104b7
leonardocaranti <51086800+leonardocaranti@users.noreply.github.com>
2019-12-02 11:03:26 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -9,26 +9,30 @@ def force_diagrams(graph_name, force, xspan, moment=0):
     plt.subplot(121)
     plt.plot(xspan,force)
     plt.title("Shear force diagram")
-    plt.xlabel("x-coordinate span (m)")
-    plt.ylabel("Shear force (N)")
+    plt.xlabel("Spanwise location from root [m]")
+    plt.ylabel("Shear force [N]")
     plt.grid()
     if abs(max(force))>abs(min(force)):
         crit_val = max(force)
     else:
         crit_val = min(force)
+    ind = force.index(crit_val)
     plt.axhline(y=crit_val, lw=1 ,ls='dashed' ,color='#d62728')
+    plt.text(xspan[ind], crit_val, str(round(crit_val)) + " [N]")
 
-    if not moment ==0:
+    if not moment == 0:
         plt.subplot(122)
         plt.plot(xspan,moment)
         plt.title("Bending moment diagram")
-        plt.xlabel("x-coordinate span (m)")
-        plt.ylabel("Bending moment (Nm)")
+        plt.xlabel("Spanwise location from root [m]")
+        plt.ylabel("Bending moment [Nm]")
         plt.grid()
         if abs(max(moment)) > abs(min(moment)):
             crit_val = max(moment)
         else:
             crit_val = min(moment)
+        ind = moment.index(crit_val)
         plt.axhline(y=crit_val, lw=1 ,ls='dashed' ,color='#d62728')
+        plt.text(xspan[ind], crit_val, str(round(crit_val)) + " [Nm]")
 
     plt.show()
